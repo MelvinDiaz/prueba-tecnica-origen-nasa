02474911bf760a0ade5c4de635cf193a3b9e822f
MelvinDiaz <melvin.diazayala@proton.me>
2025-04-28 16:04:38 -0600

    Add: pruebaEntity structure

--- src/main/java/com/nasa/prueba/aspirante/dominio/entities/PruebaEntity.java
+++ src/main/java/com/nasa/prueba/aspirante/dominio/entities/PruebaEntity.java
@@ -1,5 +1,38 @@
 package com.nasa.prueba.aspirante.dominio.entities;
 
-public class PruebaEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class PruebaEntity {
+    
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Integer id;
+    //href
+    @Column(nullable = false)
+    private String href;
+    //center
+    @Column(nullable = false)
+    private String center;
+    //title
+    @Column(nullable = false)
+    private String title;
+    //nasa_id
+    @Column(nullable = false, unique = true)
+    private String nasaId;
+
+    //date_time
+    @Column(nullable = false)
+    private LocalDateTime dateTimeCreation;
 }
